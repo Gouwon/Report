15dea946e3cef5371aabc7bdabe6c9935574a24f
Gouwon <44748728+Gouwon@users.noreply.github.com>
2018-12-18 16:32:41 +0900

    Update 181218_sql_test1.sql

--- 181218_sql_test1.sql
+++ 181218_sql_test1.sql
@@ -2,13 +2,14 @@
 
 select stu.id as '학번', max(stu.name) as '학생명', count(*) as '수강과목수', round((avg(g.midterm + g.finalterm) / 2), 1) as '전과목 평균점수'
 	from Enroll as e inner join Grade as g on e.id = g.enroll
-					 inner join Student as stu on stu.id = e.student
+		         inner join Student as stu on stu.id = e.student
 	group by stu.id
 ;
 
 create view v_stu_grade
 	as select stu.id as '학번', max(stu.name) as '학생명', count(*) as '수강과목수', round((avg(g.midterm + g.finalterm) / 2), 1) as '전과목 평균점수'
 		from Enroll as e inner join Grade as g on e.id = g.enroll
-						 inner join Student as stu on stu.id = e.student
+				 inner join Student as stu on stu.id = e.student
 		group by stu.id;
+		
 select * from v_stu_grade;
