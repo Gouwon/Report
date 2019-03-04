55b1dffb20fe0f225a445d7bc2dd7b181c253903
Gouwon <44748728+Gouwon@users.noreply.github.com>
2019-03-04 16:36:53 +0900

    Update Data_define_0002.R

--- Data_define_0002.R
+++ Data_define_0002.R
@@ -11,6 +11,14 @@ midwest111 = cbind(midwest1, midwest11$poptotal)
 head(midwest1)
 plot(midwest111$popasian, midwest111$`midwest11$poptotal`)
 boxplot(midwest111$popasian~midwest1$state, data=midwest1, xlab = "state", ylab="popasian")
+boxplot(midwest111$`midwest11$poptotal`~midwest1$state, data=midwest1, xlab = "state", ylab="poptotal")
+aggregate(data=midwest, percasian~state, mean)
+
+## 데이터를 기준으로 살펴봤을 때, 미 중서부 지역에서 사람이 가장 많이 사는 곳과 가장 적게 사는 곳의 인구수는 5105067명과 1701명이고, 아시아계의 경우는 한 명도 살지 않는 곳에서부터 188565명이 사는 곳도 있었다.
+## 437개의 county를 통합하여 5개주를 중심으로 분석을 하자면, 전체 인구수 순으로는 오하이오(OH), 일리노이(IL), 미시건(MI), 위스콘신(WI), 인디애나(IN)였다.
+## 반면 아시아계 인구 거주순으로 보았을 때, 일리노이, 미시건, 오하이오, 위스콘신, 인디애나의 순서를 보였다. 
+## 각 주별 아시아게 인구의 비율을 비교해보면 일리노이가 56.3%, 위스콘신 55.6%, 미시건 50.7%, 오하이오 43.3%, 인디애나 38.3%를 나타냈다.
+
 
 
 # poptotal 변수(컬럼)를 total로, popasian 변수를 asian으로 변수명을 변경하는 코드를 작성하시오.
@@ -34,6 +42,19 @@ hist(midwest22$asianpct, breaks = 500)
 head(midwest2)
 midwest21 = aggregate(data=midwest2, asianpct~county, mean)
 head(midwest21[order(-midwest21$asianpct),]) 
+
+midwest22 = midwest2[midwest2$asianpct > 1,]
+hist(midwest22$asianpct, breaks = 10)
+## 상기한 것을 제외한 분석결과, 아시아계 전체 인구의 1% 이상이 거주하는 곳은 15곳이었다.
+## 이 중에서 asianpct가 가장 높은 상위 5개 county는 COOK, DU PAGE, OAKLANK, CUYAHOGA, MILWAUKEE, WASHTENAW 이다.
+## 나머지 대다수 county는 1% 이하인 곳이었다. 
+
+midwest22 = midwest2[midwest2$asianpct < 0.01,]
+dim(midwest22)
+hist(midwest22$asianpct, breaks =30)
+## asianpct가 0.01% 이하인 곳만 추려도 165개의 county가 있었으며, 가장 최하위 5개의 county는 MENOMINEE(0%), BENTON(0.01746197%), IRON(0.03492394%), SCOTT(0.05238592%), CARROLL(0.05238592%)이었다.
+
+
 
 # 아시아계 인구 백분율(asianpct)의 전체 평균을 구하고, 평균을 초과하면 "lg", 그 외는 "sm"을 부여하는 파생변수(asianrate)를 추가하는 코드를 작성하시오.
 
